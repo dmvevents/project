e40d1b09e3f3666fc15d22914655407d9ca66f4e
monkmonke <eric.monk.mil@gmail.com>
2015-02-21 12:19:54 -0500

    Update Product.java
    Hey guys, I figured that the product portion should include the ID, name, price, and priority that the product is on the wishlist. Let me know what else should be added. I'm working on the wishlist now. I think Anton has got be sick though Google Chat lol.

--- SourceFiles/Classes/Product.java
+++ SourceFiles/Classes/Product.java
@@ -1,4 +1,79 @@
 
 public class Product {
 
+    private int productID;
+    private String name;
+    private int price;
+    private String priority;
+
+
+    //default constructor
+
+    public Product(){
+    }
+
+    public Product (int a, String b, int c, String d){
+
+        productID = a;
+        name = b;
+        price = c;
+        priority = d;
+    }
+
+    //toString Method
+
+   @Override
+   public String toString(){
+
+        return productID + ", " +name+ ", " + price + ", " + priority;
+   } 
+
+   //Getter Methods
+
+     public int getProductID () {
+
+        return productID;
+     }        
+
+    public String getName () {
+
+        return name;
+    }
+
+    public int getPrice () {
+
+        return price;
+    }
+
+    public String getPriority() {
+
+        return priority; 
+    }
+
+    //Setter Methods
+
+    public void setProductID(int venueID) {
+
+        this.productID = venueID;
+    }
+
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+    public void setPrice(int price) {
+
+        this.price = price;
+
+    }
+
+    public void setPriority(String priority) {
+
+        this.priority = priority;
+
+    }
+
 }
