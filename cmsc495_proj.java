c7d085033722cba5e84193aa889793fd0e85122f
monkmonke <eric.monk.mil@gmail.com>
2015-02-22 22:09:57 -0500

    Update and rename SourceFiles to cmsc495_proj.java

---
+++ cmsc495_proj.java
@@ -0,0 +1,18 @@
+package cmsc495_proj;
+
+import javax.swing.*;
+import java.io.IOException;
+import javax.swing.*;
+
+public class CMSC495_Final_Proj {
+
+
+    public static void main(String[] args) {
+       
+        CMSC495_Final_Proj window = new CMSC495_Final_Proj();
+    }
+    
+    public CMSC495_Final_Proj() {
+        Proj_Window newBox = new Proj_Window();
+    }
+}
